b5dd8d179d6c5af1c673e9e4a13ffe0848eb873e
Payrav <payrav-343@mail.ru>
2020-06-03 14:50:17 +0500

    added interfaces

---
+++ CarMarket/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,15 @@
+﻿using CarMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Data.Interfaces
+{
+    public interface IAllCars
+    {
+        IEnumerable<CarModel> Cars { get; }
+        IEnumerable<CarModel> getFavCars { get; set; }
+        CarModel getObjectCar(int carId);
+    }
+}
---
+++ CarMarket/Data/Interfaces/ICarsCategory.cs
@@ -0,0 +1,13 @@
+﻿using CarMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Data.Interfaces
+{
+    public interface ICarsCategory
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
