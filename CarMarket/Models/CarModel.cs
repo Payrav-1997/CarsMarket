5b75a07812f0472f24fed09d1616da076efcd9c1
Payrav <payrav-343@mail.ru>
2020-06-03 14:36:11 +0500

    added CarModel

---
+++ CarMarket/Models/CarModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Models
+{
+    public class CarModel
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string shortDesc { get; set; }
+        public string longDesk { get; set; }
+        public string img { get; set; }
+        public ushort price { get; set; }
+        public bool isFavorite { get; set; }
+        public bool available { get; set; }
+        public int categoryId { get; set; }
+       
+    }
+}
