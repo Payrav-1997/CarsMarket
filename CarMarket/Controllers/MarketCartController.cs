0998f6de37956b6c49b9e935438ab7ab5d17d901
Payrav <payrav-343@mail.ru>
2020-06-06 18:57:47 +0500

    added  MarketCartController end MarketCartViewModel

---
+++ CarMarket/Controllers/MarketCartController.cs
@@ -0,0 +1,35 @@
+﻿using CarMarket.Data.Models;
+using CarMarket.Data.Repository;
+using CarMarket.Views.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Controllers
+{
+    public class MarketCartController : Controller
+    {
+        private readonly CarRepository _carRepository;
+        private readonly Cart _cart;
+
+        public MarketCartController(CarRepository carRep, Cart cart)
+        {
+            _carRepository = carRep;
+            _cart = cart;
+        }
+        public ViewResult Index()
+        {
+            var items = _cart.getMarketItems();
+            _cart.listCartItems = items;
+
+            var obj = new MarketCartViewModel
+            {
+                cart = _cart
+            };
+            return View(obj);
+
+        }
+    }
+}
---
+++ CarMarket/Views/ViewModels/MarketCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CarMarket.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarMarket.Views.ViewModels
+{
+    public class MarketCartViewModel
+    {
+        public Cart cart { get; set; }
+    }
+}
